aa437e8c7738d00dffe9bb40a1a6a1670ca226c0
Christian Negron <cnegrondev@gmail.com>
2024-04-12 15:28:09 -0400

    Added routes

--- src/CheckpointServiceProvider.php
+++ src/CheckpointServiceProvider.php
@@ -20,6 +20,7 @@ class CheckpointServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('0001_01_01_000000_create_permission_tables')
-            ->hasCommand(CheckpointCommand::class);
+            ->hasCommand(CheckpointCommand::class)
+            ->hasRoute('web');
     }
 }
