d6803511589201bb7009df983405dd5e75ac5e72
Christian Negron <cnegrondev@gmail.com>
2024-04-12 16:22:36 -0400

    Testing routes

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import CheckpointLayout from "../Layouts/CheckpointLayout.vue";
+</script>
+
+<template>
+    <CheckpointLayout>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    </CheckpointLayout>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/checkpoint', 'Checkpoint/Layouts/CheckpointLayout');
+
+Route::inertia('/checkpoint', 'Checkpoint/Home');
+Route::inertia('/checkpoint-home', 'Home');
