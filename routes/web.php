b42570b3ff605b57f51e889184ccb79bef3fc2b2
Christian Negron <cnegrondev@gmail.com>
2024-04-19 14:08:41 -0400

    Test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+//Inertia::setRootView('Layouts.Checkpoint');
 Route::inertia('/checkpoint', 'Checkpoint/Home');
 
 //Route::get('/checkpoint', 'Checkpoint/Home');
